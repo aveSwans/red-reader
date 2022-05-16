5b111e3504ae1c87a0e09281a7a9c756cf358ea6
Avery Swanson <avery.swans@gmail.com>
2022-05-16 17:18:57 +0200

    basic post component ready

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -2,19 +2,28 @@ import React from 'react';
 import upArrow from '../../assets/upArrow.png';
 import downArrow from '../../assets/downArrow.png';
 import commentIcon from '../../assets/commentIcon.png';
+import sample from '../../assets/sample.png';
 
 export const Post = () => {
 
     return (
         <div className='post'>
             <h1>Post title</h1>
-            <img></img>
-            <input className='upArrow' type='image' src={upArrow}/>
-            <p className='karma'></p>
-            <input className='downArrow' type='image' src={downArrow}/>
-            <div className='author'>Iwrote Thispost</div>
-            <div className='timestamp'>10 days</div>
-            <div className='comments'><img src={commentIcon}/>1200</div>
+            <div className='karma'>
+                <input className='arrow' type='image' src={upArrow}/>
+                <p className='karmaPoints'>100</p>
+                <input className='arrow' type='image' src={downArrow}/>
+            </div>
+
+            <img src={sample}></img>
+            <div className='postFooter'>
+                <div className='author'>Timothy McMurray</div>
+                <div className='timestamp'>10 days</div>
+                <div className='comments'>
+                    <img src={commentIcon}/>
+                    <div className='commentCount'>120</div>
+                </div>
+            </div>
         </div>
     )
 }
